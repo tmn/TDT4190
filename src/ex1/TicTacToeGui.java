3580ec5b1f0f109ab95b8028cf0259d243fe2bef
Tri M. Nguyen <mail@trimn.net>
2013-02-14 19:59:03 +0100

    Add newGame function-call to remote

--- src/ex1/TicTacToeGui.java
+++ src/ex1/TicTacToeGui.java
@@ -179,9 +179,15 @@ public class TicTacToeGui extends JFrame implements Constants, ActionListener
 	 */
 	public void newGame() {
 		// This method must be modified!
+		
 		if(JOptionPane.showConfirmDialog(this, "Are you sure you want to start a new game?", "Start over?", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION)
 		{
 			clearBoard();
+			try {
+				server.newGame();
+			} catch (RemoteException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
