a1464de15b8ed020bceb7a394c37df3263fe4fbc
Tri M. Nguyen <mail@trimn.net>
2013-04-05 12:48:35 +0200

    Initial commit for ex4

---
+++ src/ex4/VentLitt.java
@@ -0,0 +1,13 @@
+package ex4;
+
+public class VentLitt
+{
+	public static void main(String[] args) {
+		long wait = 100;
+		if(args.length > 0)
+			wait = new Long(args[0]).longValue();
+		try	{
+			Thread.sleep(wait);
+		} catch (InterruptedException ie)	{}
+	}
+}
